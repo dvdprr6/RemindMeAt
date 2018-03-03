9e888806ef3a4b5c3e5f96c6e31f454339d4838a
David Parr <dvdprr6@gmail.com>
2018-03-03 14:19:18 -0500

    define fragment and placeholder fragment

--- app/src/main/java/com/example/david/remindmeat/RemindMeAtListActivity.java
+++ app/src/main/java/com/example/david/remindmeat/RemindMeAtListActivity.java
@@ -1,36 +1,65 @@
 package com.example.david.remindmeat;
 
+import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class RemindMeAtListActivity extends AppCompatActivity {
-    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remind_me_at_list);
 
-        String[] students = {"obi", "mirna", "david", "cristal", "haythem"};
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.layout_list_text_view, students);
-
-        listView = findViewById(R.id.myListView);
-
-        listView.setAdapter(arrayAdapter);
-
-        listView.setOnItemClickListener(new ListView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id){
-                TextView textView = view.findViewById(R.id.listTextView);
-                Toast.makeText(RemindMeAtListActivity.this, textView.getText().toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.EmptyContainerInSingleViewOfRemindMeAtListActivity, new PlaceholderFragment())
+                    .commit();
+        }
     }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {}
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+            String[] forecastArray = {
+                    "Today-Sunny 50/60",
+                    "Tomorrow-Cloudy 20/30",
+                    "Wednesday-Snowy 40/50",
+                    "Thursday-Rainy 20/40",
+                    "Friday-Funny 20/50",
+                    "Sat-Sunny 70/80",
+                    "Sun-Sunny 90/100"
+            };
+
+            List<String> weekForecast = new LinkedList(Arrays.asList(forecastArray));
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.layout_list_text_view, R.id.listTextView, weekForecast);
+
+            View rootView = inflater.inflate(R.layout.list_view_fragment, container, false);
+
+            ListView listView = rootView.findViewById(R.id.RemindMeAtlistview);
+
+            listView.setAdapter(adapter);
+
+            return rootView;
+
+        }
+    }
+
 }
