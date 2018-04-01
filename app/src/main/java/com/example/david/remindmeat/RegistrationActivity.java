d569f1a64e93e69eabd7bd5715ea7f97f523d8b7
David Parr <dvdprr6@gmail.com>
2018-03-31 22:08:27 -0400

    register the user

--- app/src/main/java/com/example/david/remindmeat/RegistrationActivity.java
+++ app/src/main/java/com/example/david/remindmeat/RegistrationActivity.java
@@ -2,9 +2,14 @@ package com.example.david.remindmeat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
 
-public class RegistrationActivity extends AppCompatActivity {
+import com.example.david.remindmeat.implementation.UserImplementation;
+import com.example.david.remindmeat.model.User;
+
+public class RegistrationActivity extends AppCompatActivity{
+    private static final String TAG = "RegistrationActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +18,31 @@ public class RegistrationActivity extends AppCompatActivity {
 
 
     }
+
+    public void registerUser(View view){
+        EditText editTextFirstName = findViewById(R.id.firstname);
+        EditText editTextLastName = findViewById(R.id.lastname);
+        EditText editTextEmail = findViewById(R.id.email);
+        EditText editTextPassword = findViewById(R.id.password);
+
+        String firstName = editTextFirstName.getText().toString();
+        String lastName = editTextLastName.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+
+        final UserImplementation userImplementation = new UserImplementation(this);
+
+        User user = new User();
+        user.setUserId(null);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        userImplementation.createUser(user);
+
+
+    }
+
 }
