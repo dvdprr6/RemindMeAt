504a3454e9bd8fabadb42abca584ad95fd574d96
David Parr <dvdprr6@gmail.com>
2018-03-31 22:09:55 -0400

    prevent thread blocking during persistance

--- app/src/main/java/com/example/david/remindmeat/dao/UserDao.java
+++ app/src/main/java/com/example/david/remindmeat/dao/UserDao.java
@@ -10,14 +10,16 @@ import com.example.david.remindmeat.model.User;
 
 import java.util.List;
 
+import io.reactivex.Maybe;
+
 @Dao
 public interface UserDao {
 
     @Query("SELECT * FROM users")
-    public List<User> select();
+    public Maybe<List<User>> select();
 
     @Query("SELECT * FROM users WHERE user_id=:userId")
-    public User selectById(String userId);
+    public Maybe<User> selectById(String userId);
 
     @Insert
     public void insert(User user);
