87d393734e5015df5d0586769078d95e67b217d0
David Parr <dvdprr6@gmail.com>
2018-04-10 21:22:33 -0400

    table for items to be remembered

--- app/src/main/java/com/example/david/remindmeat/table/RemindTable.java
+++ app/src/main/java/com/example/david/remindmeat/table/RemindTable.java
@@ -1,4 +1,30 @@
 package com.example.david.remindmeat.table;
 
 public class RemindTable {
+    public static final String TABLE_REMIND = "REMIND";
+    public static final String COLUMN_ID = "remind_id";
+    public static final String COLUMN_REF_USER_ID = "user_id";
+    public static final String COLUMN_DESCRIPTION = "description";
+    public static final String COLUMN_LONGITUDE = "longitude";
+    public static final String COLUMN_LATITUDE = "latitude";
+
+    public static final String[] ALL_COLUMNS = {
+            COLUMN_ID,
+            COLUMN_REF_USER_ID,
+            COLUMN_DESCRIPTION,
+            COLUMN_LONGITUDE,
+            COLUMN_LATITUDE,
+    };
+
+    public static final String SQL_CREATE =
+            "CREATE TABLE " + TABLE_REMIND + "(" +
+                    COLUMN_ID + " TEXT PRIMARY KEY," +
+                    COLUMN_DESCRIPTION + " TEXT," +
+                    COLUMN_LONGITUDE + " REAL," +
+                    COLUMN_LATITUDE + " REAL," +
+                    COLUMN_REF_USER_ID + " REFERENCES " + UserTable.TABLE_USERS + "(" + UserTable.COLUMN_ID + ")" +
+                    ");";
+
+    public static final String SQL_DELETE =
+            "DROP TABLE " + TABLE_REMIND;
 }
