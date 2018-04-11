dc168510511d5b19bca9359da853656ef8ec9b07
David Parr <dvdprr6@gmail.com>
2018-04-10 21:19:59 -0400

    constants to be used throught the application

--- app/src/main/java/com/example/david/remindmeat/utils/Constants.java
+++ app/src/main/java/com/example/david/remindmeat/utils/Constants.java
@@ -1,4 +1,10 @@
 package com.example.david.remindmeat.utils;
 
 public class Constants {
+    public static final String LOGIN_FAILED_LOGIN_FAILED = "Login Failed";
+    public static final String LOGIN_FAILED_EMAIL = "Email Already Used";
+
+    public static final String TEXT_EDIT_MISSING_INPUT_FEILDS = "Please enter all necessary information";
+
+    public static final long SQL_INSERT_FAILED = -1;
 }
