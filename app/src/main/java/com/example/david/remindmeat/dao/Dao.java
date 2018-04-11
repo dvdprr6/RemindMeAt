2a5ae389ca45b01032e7e9d4c7feac52cad2c730
David Parr <dvdprr6@gmail.com>
2018-04-10 22:38:38 -0400

    use models that only extend Item

--- app/src/main/java/com/example/david/remindmeat/dao/Dao.java
+++ app/src/main/java/com/example/david/remindmeat/dao/Dao.java
@@ -1,10 +1,10 @@
 package com.example.david.remindmeat.dao;
 
-import android.os.Parcelable;
+import com.example.david.remindmeat.model.Item;
 
 import java.util.List;
 
-public interface Dao<ITEM extends Parcelable> {
+public interface Dao<ITEM extends Item> {
     public List<ITEM> select();
     public ITEM selectById(String id);
     public long insert(ITEM object);
