5f8841d44c693fd248460f2708a2e075aac501e0
David Parr <dvdprr6@gmail.com>
2018-04-11 21:36:51 -0400

    search user by email

--- app/src/main/java/com/example/david/remindmeat/dao/UserDao.java
+++ app/src/main/java/com/example/david/remindmeat/dao/UserDao.java
@@ -4,4 +4,5 @@ import com.example.david.remindmeat.model.UserItem;
 
 public abstract class UserDao implements Dao<UserItem> {
     public abstract boolean isUser(String email, String password);
+    public abstract UserItem searchUserByEmail(String email);
 }
