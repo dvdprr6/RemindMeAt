4506e78010a3785c1a3e11fd79b4d9749bb28ef7
David Parr <dvdprr6@gmail.com>
2018-04-11 21:34:47 -0400

    to persist the db only

--- app/src/main/java/com/example/david/remindmeat/dao/RemindItemDao.java
+++ app/src/main/java/com/example/david/remindmeat/dao/RemindItemDao.java
@@ -1,4 +1,44 @@
 package com.example.david.remindmeat.dao;
 
-public class RemindItemDao {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.david.remindmeat.database.DatabaseHelper;
+import com.example.david.remindmeat.model.RemindItem;
+
+import java.util.List;
+
+public class RemindItemDao extends RemindDao {
+    private SQLiteOpenHelper databaseHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
+    public RemindItemDao(Context context){
+        databaseHelper = DatabaseHelper.getInstance(context);
+    }
+
+    @Override
+    public List<RemindItem> select() {
+        return null;
+    }
+
+    @Override
+    public RemindItem selectById(String id) {
+        return null;
+    }
+
+    @Override
+    public long insert(RemindItem remindItem) {
+        return 0;
+    }
+
+    @Override
+    public void update(RemindItem remindItem) {
+
+    }
+
+    @Override
+    public void delete(RemindItem remindItem) {
+
+    }
 }
