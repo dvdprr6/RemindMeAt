2455d36423b5281247a8be0e935f7cb5d31173ac
David Parr <dvdprr6@gmail.com>
2018-04-14 19:14:11 -0400

    implement list click handler

--- app/src/main/java/com/example/david/remindmeat/handler/ListClickHandler.java
+++ app/src/main/java/com/example/david/remindmeat/handler/ListClickHandler.java
@@ -1,6 +1,27 @@
 package com.example.david.remindmeat.handler;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 import android.widget.AdapterView;
 
+import com.example.david.remindmeat.UpdateRemindMeItemActivity;
+import com.example.david.remindmeat.model.RemindItem;
+import com.example.david.remindmeat.utils.Constants;
+
 public class ListClickHandler implements AdapterView.OnItemClickListener {
+    private Context context;
+
+    public ListClickHandler(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        RemindItem remindItem = (RemindItem) parent.getItemAtPosition(position);
+
+        Intent remindMeItemUpdateIntent = new Intent(this.context, UpdateRemindMeItemActivity.class);
+        remindMeItemUpdateIntent.putExtra(Constants.EXTRA_SELECTED_REMIND_ME_ITEM, remindItem);
+        this.context.startActivity(remindMeItemUpdateIntent);
+    }
 }
