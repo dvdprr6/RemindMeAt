3c5701d6461caca065bc77a274296fe1e8886bf8
David Parr <dvdprr6@gmail.com>
2018-04-14 15:02:34 -0400

    implement find items by user abstract method

--- app/src/main/java/com/example/david/remindmeat/dao/RemindItemDao.java
+++ app/src/main/java/com/example/david/remindmeat/dao/RemindItemDao.java
@@ -2,13 +2,16 @@ package com.example.david.remindmeat.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.david.remindmeat.database.DatabaseHelper;
+import com.example.david.remindmeat.model.Item;
 import com.example.david.remindmeat.model.RemindItem;
 import com.example.david.remindmeat.table.RemindTable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RemindItemDao extends RemindDao {
@@ -47,5 +50,39 @@ public class RemindItemDao extends RemindDao {
     @Override
     public void delete(RemindItem remindItem) {
 
+    }
+
+    @Override
+    public List<Item> findRemindItemsByUserId(String userId) {
+        List<Item> remindItems = new ArrayList();
+        sqLiteDatabase = databaseHelper.getReadableDatabase();
+
+        Cursor cursor = sqLiteDatabase.query(
+                RemindTable.TABLE_REMIND,
+                RemindTable.ALL_COLUMNS,
+                RemindTable.COLUMN_REF_USER_ID + "= ?",
+                new String[]{userId},
+                null, null, null);
+
+        while(cursor.moveToNext()){
+            RemindItem remindItem = new RemindItem();
+            String id = cursor.getString(cursor.getColumnIndex(RemindTable.COLUMN_ID));
+            String title = cursor.getString(cursor.getColumnIndex(RemindTable.COLUMN_TITLE));
+            String description = cursor.getString(cursor.getColumnIndex(RemindTable.COLUMN_DESCRIPTION));
+            double latitude = cursor.getDouble(cursor.getColumnIndex(RemindTable.COLUMN_LATITUDE));
+            double longitude = cursor.getDouble(cursor.getColumnIndex(RemindTable.COLUMN_LONGITUDE));
+            userId = cursor.getString(cursor.getColumnIndex(RemindTable.COLUMN_REF_USER_ID));
+
+            remindItem.setId(id);
+            remindItem.setTitle(title);
+            remindItem.setDescription(description);
+            remindItem.setLatitude(latitude);
+            remindItem.setLongitude(longitude);
+            remindItem.setUserId(userId);
+
+            remindItems.add(remindItem);
+        }
+
+        return remindItems;
     }
 }
