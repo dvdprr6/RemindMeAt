26ebca878adaa1a188f3fd21242592539dea7c1d
David Parr <dvdprr6@gmail.com>
2018-04-22 12:22:58 -0400

    implement the intent service for notification

--- app/src/main/java/com/example/david/remindmeat/service/LocationAlertIntentService.java
+++ app/src/main/java/com/example/david/remindmeat/service/LocationAlertIntentService.java
@@ -1,91 +1,151 @@
 package com.example.david.remindmeat.service;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
- */
+import com.example.david.remindmeat.R;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+
 public class LocationAlertIntentService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "com.example.david.remindmeat.service.action.FOO";
-    private static final String ACTION_BAZ = "com.example.david.remindmeat.service.action.BAZ";
+    private static final String IDENTIFIER = "LocationAlertIS";
 
-    // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "com.example.david.remindmeat.service.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "com.example.david.remindmeat.service.extra.PARAM2";
-
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, LocationAlertIntentService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, LocationAlertIntentService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    public LocationAlertIntentService() {
-        super("LocationAlertIntentService");
+    public LocationAlertIntentService(){
+        super(IDENTIFIER);
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
-            }
+    protected void onHandleIntent(@Nullable Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+
+        if(geofencingEvent.hasError()){
+            Log.e(IDENTIFIER, "" + getErrorString(geofencingEvent.getErrorCode()));
+            return;
+        }
+
+        Log.i(IDENTIFIER, geofencingEvent.toString());
+
+        int geofencingTransition = geofencingEvent.getGeofenceTransition();
+
+        if(geofencingTransition == Geofence.GEOFENCE_TRANSITION_ENTER || geofencingTransition == Geofence.GEOFENCE_TRANSITION_DWELL){
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            String transitionDetails = getGeofencingTransitionInfo(triggeringGeofences);
+
+            String transitionType = getTransitionString(geofencingTransition);
+
+            notifyLocationAlert(transitionType, transitionDetails);
         }
     }
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
+    private String getGeofencingTransitionInfo(List<Geofence> triggeringGeofences){
+        List<String> locationNames = new ArrayList();
+        for(Geofence geofence : triggeringGeofences){
+            locationNames.add(getLocationName(geofence.getRequestId()));
+        }
+
+        String triggeringLocationString = TextUtils.join(", ", locationNames);
+
+        return triggeringLocationString;
     }
 
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
+    private String getLocationName(String key){
+        String[] strs = key.split("-");
+
+        String locationName = null;
+
+        if(strs != null && strs.length == 2){
+            double lat = Double.parseDouble(strs[0]);
+            double lng = Double.parseDouble(strs[1]);
+
+            locationName = getLocationNameGeocoder(lat, lng);
+        }
+
+        if(locationName != null){
+            return locationName;
+        }else{
+            return key;
+        }
+    }
+
+    private String getLocationNameGeocoder(double lat, double lng){
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+
+        try{
+            addresses = geocoder.getFromLocation(lat, lng, 1);
+        }catch(Exception ioException){
+            Log.e("", "Error in getting location name for the location");
+        }
+
+        if(addresses == null || addresses.size() == 0){
+            Log.d("", "no location name");
+            return null;
+        }else{
+            Address address = addresses.get(0);
+            ArrayList<String> addressInfo = new ArrayList();
+            for(int i = 0; i <= address.getMaxAddressLineIndex(); i++){
+                addressInfo.add(address.getAddressLine(i));
+            }
+
+            return TextUtils.join(System.getProperty("line.separator"), addressInfo);
+        }
+    }
+
+    private String getErrorString(int errorCode){
+        switch(errorCode){
+            case GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE:
+                return "Geofence not avilable";
+            case GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES:
+                return "Geofence too many_geofences";
+            case GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS:
+                return "Geofence too many pending_intents";
+            default:
+                return "geofence error";
+        }
+    }
+
+    private String getTransitionString(int transitionType){
+        switch(transitionType){
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                return "location entered";
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                return "located exited";
+            case Geofence.GEOFENCE_TRANSITION_DWELL:
+                return "dwell at location";
+            default:
+                return "location transition";
+        }
+    }
+
+    private void notifyLocationAlert(String locTransitionType, String locationDetails) {
+
+        String CHANNEL_ID = "Zoftino";
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle(locTransitionType)
+                        .setContentText(locationDetails);
+
+        builder.setAutoCancel(true);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(0, builder.build());
     }
 }
