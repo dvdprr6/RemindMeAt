98c21328517ea98a44508ce842e3c0491afa2b8e
David Parr <dvdprr6@gmail.com>
2018-04-22 12:23:37 -0400

    geofence parameters

--- app/src/main/java/com/example/david/remindmeat/utils/Constants.java
+++ app/src/main/java/com/example/david/remindmeat/utils/Constants.java
@@ -13,4 +13,10 @@ public class Constants {
     public static final String EXTRA_SELECTED_REMIND_ME_ITEM = "extra_selected_remind_me_item";
 
     public static final String TOAST_FILL_FIELDS = "Please fill all fields";
+
+    public static final String LOCATION_INTENT_SERVICE_INDENTIFIER = "LocationAlertIS";
+
+    public static final int GEOFENCE_RADUIS = 500;
+
+    public static final int GEOFENCE_EXPIRATION = 6000;
 }
